5d6f99971e988161b01f1691575339a061baef1f
KenTandrian <kenricktan11@gmail.com>
2023-04-01 20:00:37 +0700

    fix: test config

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { INFO } from 'src/helpers';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,9 +17,6 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect(INFO);
   });
 });
