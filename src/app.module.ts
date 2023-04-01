dad65d453dba769ce077ebfd762585f43b73f375
KenTandrian <kenricktan11@gmail.com>
2023-04-01 16:47:48 +0700

    feat: add PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [],
   providers: [],
 })
