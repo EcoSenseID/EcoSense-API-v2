cd2ae08aa199354d071d0759d0da2f2329b6c6ff
KenTandrian <kenricktan11@gmail.com>
2023-04-01 19:11:27 +0700

    feat: add more endpoints to campaigns

--- src/campaigns/campaigns.controller.ts
+++ src/campaigns/campaigns.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CampaignsService } from './campaigns.service';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
-@ApiTags('campaigns')
+@ApiTags('Campaigns')
 @Controller('campaigns')
 export class CampaignsController {
   constructor(private campaignsService: CampaignsService) {}
@@ -22,5 +35,34 @@ export class CampaignsController {
   @ApiOkResponse({ description: 'Campaign details fetched successfully' })
   findOne(@Param('id', ParseIntPipe) campaignId: number) {
     return this.campaignsService.findOne(campaignId);
+  }
+
+  @Get('my')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get all campaigns made by the user' })
+  @ApiOkResponse({ description: 'Campaigns fetched successfully' })
+  findMy() {
+    return 'This action returns all campaigns made by the user';
+  }
+
+  @Post('')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create a new campaign' })
+  create() {
+    return 'This action adds a new campaign';
+  }
+
+  @Put(':id')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update a campaign' })
+  update(@Param('id') id: string) {
+    return `This action updates a #${id} campaign`;
+  }
+
+  @Delete(':id')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete a campaign' })
+  delete(@Param('id') id: string) {
+    return `This action deletes a #${id} campaign`;
   }
 }
