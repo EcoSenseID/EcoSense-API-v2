f751a3eb16e70d4fb1a10fd6943ccdf1c795f454
KenTandrian <kenricktan11@gmail.com>
2023-04-02 14:19:24 +0700

    fix: failing test cases

--- src/notification/notification.controller.spec.ts
+++ src/notification/notification.controller.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificationController } from './notification.controller';
+import { NotificationService } from './notification.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('NotificationController', () => {
   let controller: NotificationController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [NotificationService, PrismaService, ConfigService],
       controllers: [NotificationController],
     }).compile();
 
